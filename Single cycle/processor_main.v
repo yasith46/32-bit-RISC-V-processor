8f594c3c26e94ca016cc821ded0c7a68fc015668
Yasith Silva <133279208+yasith46@users.noreply.github.com>
2024-10-23 16:59:58 +0530

    Update processor_main.v

--- Single cycle/processor_main.v
+++ Single cycle/processor_main.v
@@ -1,18 +1,18 @@
 //////////////////////////////////////////////////////////////////////////////////
-// Group: 				MetroniX 
-// Designer: 			Bimsara Nawarathne, Yasith Silva
+// Group: 		MetroniX 
+// Designer: 		Bimsara Nawarathne, Yasith Silva
 // 
 // Create Date:    	11:29:15 17/10/2024 
-// Design Name: 	 	Processor (Top module)
+// Design Name: 	Processor (Top module)
 // Module Name:    	processor_main 
 // Project Name:   	32 bit Single Cycle RISC-V processor
 // Target Devices: 	Altera Cyclone IV EP4CE115F29 (DE2-115)
 //
 // Dependencies: 
 //
-// Revision: 			3
+// Revision: 		3
 // Additional Comments: - Add inspectbuffer the fucntionality to inspect a specific 
-//								  location 
+//			  location 
 //
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -21,28 +21,28 @@
 module processor_main(
 		input  clk, reset_n,                       // Clock signal
 		output reg [31:0] inspectbuffer
-   );
+	);
 	 
-   wire [31:0] instruct_address, instruct_address_in, PCADD4;    // Address of the next instruction
-   wire [31:0] inst;                // The instruction from instruction memory
+   	wire [31:0] instruct_address, instruct_address_in, PCADD4;    // Address of the next instruction
+   	wire [31:0] inst;                // The instruction from instruction memory
 	wire [31:0] data_rs1;
 	wire [31:0] data_rs2;
 
-   // Instantiate the ProgramCounter module
-	ProgramCounter pc(
-      .instruct_address_in(instruct_address_in),
-      .clk(clk),
+   	// Instantiate the ProgramCounter module
+	ProgramCounter PC(
+      		.instruct_address_in(instruct_address_in),
+      		.clk(clk),
 		.rst(reset_n),
 		.instruct_address(instruct_address)
-   );
+   	);
 	
 	add pcadd4(.A(instruct_address), .B(32'd4), .CIN(1'b0), .OF(), .SUM(PCADD4));
 
-   // Instantiate the InstructMem module (assume InstructMem takes address and returns instruction)
-   InstructMem imem(
-      .Pro_count(instruct_address),  // Pass the instruction address from ProgramCounter
+	// Instantiate the InstructMem module (assume InstructMem takes address and returns instruction)
+   	InstructMem imem(
+      		.Pro_count(instruct_address),  // Pass the instruction address from ProgramCounter
 		.inst_out(inst)           // Fetch the instruction corresponding to the address
-   );
+   	);
 	 
 	wire CTRL_MEMREAD, CTRL_MEMWRITE, CTRL_ALUSRC, CTRL_REGWRITE, CRTL_IMMTOREG;
 	wire [1:0] CTRL_ALUOP, CTRL_BRANCH, CTRL_REGWRITESEL;
@@ -122,12 +122,12 @@ module processor_main(
 	add pcaddimm(.A(instruct_address), .B(IMM_EXT << 1), .CIN(1'b0), .OF(), .SUM(PCADDIMM));
 	
 	assign REGWRITE_DATA = (CTRL_REGWRITESEL == 2'b00) ? CAL_OUT :
-								  (CTRL_REGWRITESEL == 2'b01) ? DMEM_OUT : 
-								  (CTRL_REGWRITESEL == 2'b10) ? PCADD4 : PCADDIMM;
+			       (CTRL_REGWRITESEL == 2'b01) ? DMEM_OUT : 
+			       (CTRL_REGWRITESEL == 2'b10) ? PCADD4 : PCADDIMM;
 								  
 	assign instruct_address_in = ({(CTRL_BRANCH[1] & ALU_BRANCHFLAG), CTRL_BRANCH[0]} == 2'b00) ? PCADD4 :
-									     ({(CTRL_BRANCH[1] & ALU_BRANCHFLAG), CTRL_BRANCH[0]} == 2'b01) ? PCADD4 :
-									     ({(CTRL_BRANCH[1] & ALU_BRANCHFLAG), CTRL_BRANCH[0]} == 2'b10) ? ALU_OUT : PCADDIMM;
+				     ({(CTRL_BRANCH[1] & ALU_BRANCHFLAG), CTRL_BRANCH[0]} == 2'b01) ? PCADD4 :
+				     ({(CTRL_BRANCH[1] & ALU_BRANCHFLAG), CTRL_BRANCH[0]} == 2'b10) ? ALU_OUT : PCADDIMM;
 										  
 	always@(clk) begin
 		inspectbuffer <= DMEM_OUT;
