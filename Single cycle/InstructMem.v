cacbf0b038f9af392263589d28c2db41cce5ddaf
Yasith Silva <133279208+yasith46@users.noreply.github.com>
2024-11-10 10:43:08 +0530

    Update InstructMem.v

--- Single cycle/InstructMem.v
+++ Single cycle/InstructMem.v
@@ -21,13 +21,13 @@ module InstructMem(
 		input [31:0] instruct_address_in,
 		output reg [31:0] instruct_address,
 		output reg [31:0] inst_out   // Instruction bus width is 32 bits
-   );
+	);
 
 	(* ramstyle = "M9K" *) reg [31:0] IM [512:0];    // Data memory with 128 locations // Can extend 128 to 2^30
 		
 	initial begin
 		IM[0]  = 32'h08000313;          	//  0: li	   t1,128
-		IM[1]  = 32'h00032383;         	//  4: lw	   t2,0(t1)
+		IM[1]  = 32'h00032383;         		//  4: lw	   t2,0(t1)
 		IM[2]  = 32'h00032383;          	//  8: stall
 		IM[3]  = 32'h3e800093;          	//  c: li	   ra,1000
 		IM[4]  = 32'h7d008113;          	// 10: addi	   sp,ra,2000
@@ -49,13 +49,12 @@ module InstructMem(
 	end
 	
 	always @(posedge clk or negedge rst) begin
-			if (rst == 1'b0) begin
-				instruct_address <= 32'b0;
-				inst_out <= IM[30'b0];  // No need to consider first 2 bits because they are used to address bytes seperately
-			end else begin 
-				instruct_address <= instruct_address_in; // Increment by 4 (word-aligned)
-				inst_out <= IM[instruct_address_in[31:2]];
-			end		
-   end
-
+		if (rst == 1'b0) begin
+			instruct_address <= 32'b0;
+			inst_out <= IM[30'b0];  // No need to consider first 2 bits because they are used to address bytes seperately
+		end else begin 
+			instruct_address <= instruct_address_in; // Increment by 4 (word-aligned)
+			inst_out <= IM[instruct_address_in[31:2]];
+		end		
+   	end
 endmodule 
